75337e64afcea652f3fe524e13a9b12ff88db661
ChrisT33850 <christophe.trevise@acteongroup.com>
2026-09-16 10:23:56 +0200

    Update supabaseClient.ts

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://dsmeitqmdfdavgayznsu.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_S8UpiAdtq809CYl3jbLLFw_ks6YomOk';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzbWVpdHFtZGZkYXZnYXl6bnN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1MTc5NTUsImV4cCI6MjEwNTA5Mzk1NX0.2BTWa5cpTRQXC9IGG5RYBg1JavV0U2qVtTry80lQERc';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
